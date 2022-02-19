00e1c17815689aa09b0c0ee02b018bd4c492a5d2
arman <gasparyanarman>
2022-02-19 13:43:11 +0400

    second commit

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-import "fmt"
+import "awesomeProject1/services"
 
 func main() {
-	x := 3
-	fmt.Println(x)
-
-	newVar := 100
-	fmt.Println(newVar)
+	var a = services.Iphone{}
+	a.Call()
 }
